1b9dfde4808997f8f5eaa8b71d30df68a5a85ae6
Manan Patel <mananpatel11@gmail.com>
2021-08-25 17:52:48 -0500

    Fixed build script

--- build.sh
+++ build.sh
@@ -1,6 +1,1 @@
-clang++ -std=c++11 app.mm -c -o app.o
-clang++ -std=c++11 -c data_types.cpp -o data_types.o
-clang++ -std=c++11 -c framebuffer.cpp -o framebuffer.o
-clang++ -std=c++11 -c main.cpp -o main.o
-clang++ -std=c++11 -c window.cpp -o window.o
-clang++ -std=c++11 -framework Cocoa app.o data_types.o main.o window.o framebuffer.o -o super-basoon
+clang++ -std=c++17 -g -framework Cocoa *.cpp app.mm -o basoon
